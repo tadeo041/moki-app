c105d3780e92891e945ed9e7c1d51185ca48e1f6
Manuel <josemanueltadeo2003@gmail.com>
2026-06-25 21:16:30 -0600

    ⬇️ Se descargaron las dependencias de tailwind

--- frontend/src/app/auth/inicio-sesion/inicio-sesion.component.ts
+++ frontend/src/app/auth/inicio-sesion/inicio-sesion.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
+  selector: 'app-inicio-sesion',  
+  standalone: true,  
+  imports: [CommonModule],  
   templateUrl: './inicio-sesion.component.html',
   styleUrls: ['./inicio-sesion.component.scss'],
 })
-export class PagueInicioSesionComponent  implements OnInit {
-
-  constructor() { }
+export class PagueInicioSesionComponent implements OnInit {
+  constructor() {}
 
   ngOnInit() {}
-
 }
